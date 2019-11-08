cbcf82b098e395049342524a7282ddc8a2ad357e
Eugene Pavlenko <eugene.p@doubledmarketing.com>
2019-11-08 16:08:25 +0200

    add prototypes for c functions

--- src/PE_mGFX.h
+++ src/PE_mGFX.h
@@ -21,6 +21,12 @@ typedef struct {
     uint8_t height;
 } PE_mGFX_Font_t;
 
+typedef struct {
+    uint8_t *data;
+    uint8_t width;
+    uint8_t height;
+} PE_mGFX_Canvas_t;
+
 typedef void (*PE_mGFX_flush_t) (const uint8_t *data, uint16_t size);
 
 class PE_mGFX {
@@ -152,5 +158,85 @@ public:
      */
     uint8_t * getBufferData();
 };
+
+/**
+ * Draw pixel
+ *
+ * @param canvas
+ * @param x
+ * @param y
+ * @param color
+ */
+void PE_mGFX_pixel(PE_mGFX_Canvas_t *canvas, uint16_t x, uint16_t y, PE_mGFX_Color_t color);
+
+/**
+ * Draw line
+ *
+ * @param canvas
+ * @param x0
+ * @param y0
+ * @param x1
+ * @param y1
+ * @param color
+ */
+void PE_mGFX_line(PE_mGFX_Canvas_t *canvas, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, PE_mGFX_Color_t color);
+
+/**
+ * Draw rectangle
+ *
+ * @param canvas
+ * @param x0
+ * @param y0
+ * @param x1
+ * @param y1
+ * @param color
+ */
+void PE_mGFX_rectangle(PE_mGFX_Canvas_t *canvas, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, PE_mGFX_Color_t color);
+
+/**
+ * Draw circle
+ *
+ * @param canvas
+ * @param cx
+ * @param cy
+ * @param r
+ * @param color
+ */
+void PE_mGFX_circle(PE_mGFX_Canvas_t *canvas, uint16_t cx, uint16_t cy, uint16_t r, PE_mGFX_Color_t color);
+
+/**
+ * Draw bitmap
+ *
+ * @param canvas
+ * @param x
+ * @param y
+ * @param bitmap
+ * @param color
+ */
+void PE_mGFX_bitmap(PE_mGFX_Canvas_t *canvas, uint16_t x, uint16_t y, PE_mGFX_Bitmap_t *bitmap, PE_mGFX_Color_t color);
+
+/**
+ * Draw single char
+ *
+ * @param canvas
+ * @param x
+ * @param y
+ * @param symbol
+ * @param font
+ * @param color
+ */
+void PE_mGFX_symbol(PE_mGFX_Canvas_t *canvas, uint16_t x, uint16_t y, char symbol, PE_mGFX_Font_t *font, PE_mGFX_Color_t color);
+
+/**
+ * Draw string
+ *
+ * @param canvas
+ * @param x
+ * @param y
+ * @param string
+ * @param font
+ * @param color
+ */
+void PE_mGFX_string(PE_mGFX_Canvas_t *canvas, uint16_t x, uint16_t y, const char *string, PE_mGFX_Font_t *font, PE_mGFX_Color_t color);
 
 #endif //PE_MGFX_H
